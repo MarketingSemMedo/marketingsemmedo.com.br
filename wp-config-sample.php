dffe170283192d4416832ad786fca78769e2bd58
diegoliacone <diegoliacone@gmail.com>
2016-03-28 13:12:28 -0300

    Instaãlação inicial (WORDPRESS 4.2, Thema e pluguins)

---
+++ wp-config-sample.php
@@ -0,0 +1,20 @@
+<?php
+	//* Configurações Locais *//
+	// ** Configurações do MySQL - Você pode pegar essas informações com o serviço de hospedagem ** //
+	/** O nome do banco de dados do WordPress */
+	define('DB_NAME', 'msm.db');
+
+	/** Usuário do banco de dados MySQL */
+	define('DB_USER', 'root');
+
+	/** Senha do banco de dados MySQL */
+	define('DB_PASSWORD', '');
+
+	/** nome do host do MySQL */
+	define('DB_HOST', 'localhost');
+
+	/** Conjunto de caracteres do banco de dados a ser usado na criação das tabelas. */
+	define('DB_CHARSET', 'utf8mb4');
+
+	/** O tipo de collate do banco de dados. Não altere isso se tiver dúvidas. */
+	define('DB_COLLATE', '');
